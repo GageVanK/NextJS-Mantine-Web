33746161166fa6f002038167a6db4ace50e01c97
gage <87737875+GageVanK@users.noreply.github.com>
2023-05-10 17:57:48 +0000

    Update 5 files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { Welcome } from '../components/Welcome/Welcome';
+import { ContactUsBox } from '../components/ContactUsBox';
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
+      <ContactUsBox />
     </>
   );
 }
